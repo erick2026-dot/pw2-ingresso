9347324fbc2c4dcdbe1313069896cfd1966466af
Benjamino <you@example.com>
2026-08-17 08:48:49 -0300

    commit 17-08

--- src/main/java/br/com/etec/ingresso/controller/FilmeController.java
+++ src/main/java/br/com/etec/ingresso/controller/FilmeController.java
@@ -3,6 +3,8 @@ package br.com.etec.ingresso.controller;
 import br.com.etec.ingresso.entity.Filme;
 import br.com.etec.ingresso.enums.ClassificacaoIndicadivaEnum;
 import br.com.etec.ingresso.enums.SimNaoEnum;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -28,20 +30,39 @@ public class FilmeController {
                 .build();
         return List.of(filme1, filme2);
     }
-
+    List<Long> idInexistente = List.of(1L, 2L, 3L);
     @GetMapping("/{id}")
-        public Filme buscarPorId(@PathVariable Long id) {
-        Filme filme1 = Filme.builder()
-                .id(id)
-                .nome("Matrix")
-                .classificacao(ClassificacaoIndicadivaEnum.A16)
-                .emCartaz(SimNaoEnum.S)
-                .build();
-        return filme1;
+        public ResponseEntity<Filme> buscarPorId(@PathVariable Long id) {
+        if(idInexistente.contains(id)) {
+            Filme filme1 = Filme.builder()
+                    .id(id)
+                    .nome("Matrix")
+                    .classificacao(ClassificacaoIndicadivaEnum.A16)
+                    .emCartaz(SimNaoEnum.S)
+                    .build();
+            return ResponseEntity.ok(filme1);
+        }
+        return ResponseEntity.notFound().build();
     }
     @PostMapping
-    public Filme cadastrar(@RequestBody Filme filme){
+    public ResponseEntity<Filme> cadastrar(@RequestBody Filme filme){
         filme.setId(199L);
-        return filme;
+        return ResponseEntity.status(HttpStatus.CREATED).body(filme);
     }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Filme> atualizar(@RequestBody Filme filme,
+                                           @PathVariable Long id){
+        if(idInexistente.contains(id)) {
+            return ResponseEntity.ok(filme);
+        }
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletar(@PathVariable Long id){
+        if(idInexistente.contains(id)){
+            return ResponseEntity.ok().build();
+        }
+        return ResponseEntity.notFound().build();
+    }
+
 }
